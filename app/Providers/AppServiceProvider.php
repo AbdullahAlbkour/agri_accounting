15830f8bb4b7b790acdf790b3ecb7e3ffff7df90
Abdullah <Abdullah@ABDULLAH-ALBKOU>
2026-09-10 13:51:53 +0300

    Fix URI scheme check in CLI

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,10 +18,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+   public function boot(): void
     {
-        if (config('app.env') === 'production' || env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
+        if (!app()->runningInConsole() && (config('app.env') === 'production' || env('APP_ENV') === 'production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
         }
     }
 }
